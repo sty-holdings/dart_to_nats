04636ae519380d208ee78f2e45aab931737f7b75
John Henry Galino <git@jhgalino.com>
2024-12-05 03:29:12 +0000

    Update launch configuration and add secure_main.dart for NATS client connection
    - Changed the launch program in launch.json to point to example/secure_main.dart.
    - Added a new secure_main.dart file that implements a secure NATS client connection using TLS, including certificate handling and message publishing/subscribing functionality.

---
+++ example/secure_main.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:dart_nats/dart_nats.dart';
+
+void main() async {
+  var client = Client();
+  final securityContext = SecurityContext()
+    ..setTrustedCertificates('../rootCA.pem')
+    ..useCertificateChain('../client-cert.pem')
+    ..usePrivateKey('../client-key.pem');
+
+  await client.connect(Uri.parse('tls://localhost:4222'),
+      securityContext: securityContext);
+
+  // print(client.info?.toJson());
+  var sub = client.sub('subject1');
+  client.pubString('subject1', 'message1');
+  var data = await sub.stream.first;
+
+  print(data.string);
+  client.unSub(sub);
+  await client.close();
+}
